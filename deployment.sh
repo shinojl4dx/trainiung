c17ce8789f121a1210bd3129c69844978e9de8ad
shinojl4dx <l4dx@narrowlabs.in>
2025-12-04 15:57:31 +0530

    hjebhejbd

--- deployment.sh
+++ deployment.sh
@@ -2,11 +2,11 @@
 # Infra Deployment
 set -e
 
-echo "Deploying Stack Jenkins_Test-Stack using parameter.json..."
+echo "Deploying Stack JenkinsTestStack using parameter.json..."
 
 aws cloudformation deploy \
   --template-file ec2-cft.yml \
-  --stack-name jenkisn_Test-Stack \
+  --stack-name jenkisnTestStack \
   --parameter-overrides file://parameter.json \
   --capabilities CAPABILITY_NAMED_IAM
 
